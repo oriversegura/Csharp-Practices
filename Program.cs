bf0de2b2217337119f4ac0773a467ac64474465d
oriversegura <oriverseguravargas@gmail.com>
2023-12-19 04:46:59 -0400

    add practices

--- Program.cs
+++ Program.cs
@@ -1,40 +1,44 @@
 ﻿
-//Practice 1 
+//Practice 1 ------------------------------------------------------------------------------------------------------------------------------------
 
 
 
-//Practice 2 
+//Practice 2 ------------------------------------------------------------------------------------------------------------------------------------
 
-Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+// Random random = new Random();
+// int daysUntilExpiration = random.Next(12);
+// int discountPercentage = 0;
 
-// Your code goes here
-if (daysUntilExpiration > 5 && daysUntilExpiration <= 10)
-{
+// // Your code goes here:
+
+// if (daysUntilExpiration > 5 && daysUntilExpiration <= 10)
+// {
     
-    Console.WriteLine("Your subscription will expire soon. Renew now!");   
-}
-else if (daysUntilExpiration > 1 && daysUntilExpiration <= 5)
-{
-    discountPercentage = 10;
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!");
-}
-else if (daysUntilExpiration == 1)
-{
-    discountPercentage = 20;
-    Console.WriteLine($"Your subscription expires within a day!\nRenew now and save {discountPercentage}%!");
-}
-else
-{
-    Console.WriteLine("Your subscription has expired.");
-}
+//     Console.WriteLine("Your subscription will expire soon. Renew now!");   
+// }
+// else if (daysUntilExpiration > 1 && daysUntilExpiration <= 5)
+// {
+//     discountPercentage = 10;
+//     Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!");
+// }
+// else if (daysUntilExpiration == 1)
+// {
+//     discountPercentage = 20;
+//     Console.WriteLine($"Your subscription expires within a day!\nRenew now and save {discountPercentage}%!");
+// }
+// else
+// {
+//     Console.WriteLine("Your subscription has expired.");
+// }
 
 
-// Practice 3
+// Practice 3 ------------------------------------------------------------------------------------------------------------------------------------
 
 
 string firstName = "Bob";
 int widgetsSold = 7;
 Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");git 
+
+
+
 
